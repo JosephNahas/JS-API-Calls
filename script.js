8a9563a9f2aa0adcb750510f1472582163005e68
Joseph Nahas <joe_nahas@hotmail.com>
2024-12-06 02:07:54 -0500

    Implemented xhr button

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ document.getElementById('fetch').addEventListener('click', function() {
         .catch(error => errorMessage.innerHTML = 'error fetching data');
 });
 
+document.getElementById('xhr').addEventListener('click', function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { 
+            if (xhr.status === 200) { 
+                const data = JSON.parse(xhr.responseText);
+                displayData(data);
+            } else {
+                errorMessage.innerHTML = 'error fetching data';
+            }
+        }
+    };
+
+    xhr.send();
+});
+
 function displayData(data) {
     const title = document.getElementById('title');
     const body = document.getElementById('body');
