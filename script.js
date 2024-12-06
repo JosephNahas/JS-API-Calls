315cff46c8ad713e1c4932c7c35587d9f6cf4c35
Joseph Nahas <joe_nahas@hotmail.com>
2024-12-06 03:53:45 -0500

    Implemented PUT

--- script.js
+++ script.js
@@ -45,18 +45,24 @@ function displayData(data) {
 let postForm = document.getElementById('form');
 let formTitle = document.getElementById('post-title');
 let formBody = document.getElementById('post-body');
+let formID = document.getElementById('post-id');
 let responseP = document.getElementById('response');
 let responseTitle = document.getElementById('response-title');
 let responseBody = document.getElementById('response-body');
+let submitButton = document.getElementById('submit-btn');
+let updateButton = document.getElementById('update-btn');
 
-postForm.addEventListener('submit', function(event){
+
+
+
+submitButton.addEventListener('click', function(event){
     event.preventDefault();
-    
+
     var title = formTitle.value;
     var body = formBody.value;
-
+    
     if ((title == '' || title.trim().length == 0 || body == '' || body.trim().length == 0 )){
-        alert('Do not leave any section of the post empty');
+        alert('Do not leave post title or body empty');
     } else{
         fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
@@ -71,11 +77,47 @@ postForm.addEventListener('submit', function(event){
         .then(function(response){
         return response.json()})
         .then(function(data){
-            console.log(data);
             errorMessage.innerHTML = '';
+            responseP.innerHTML = 'The following data was posted';
             responseP.removeAttribute('hidden');
             responseTitle.innerHTML = data.title;
             responseBody.innerHTML = data.body;
         }).catch(function(){errorMessage.innerHTML = 'error posting data'});
     }   
 });
+
+
+var url = " https://jsonplaceholder.typicode.com/posts/";
+
+updateButton.addEventListener('click', function(event){  
+    event.preventDefault();
+    var title = formTitle.value;
+    var body = formBody.value;
+    var id = formID.value;
+    if (title == '' || title.trim().length == 0 || body == '' || body.trim().length == 0 || id == '' || id.trim().length == 0){
+        alert('Do not leave any section of the form empty')
+    } else{
+        var updateData = {};
+        updateData.title = title;
+        updateData.body  = body;
+        var json = JSON.stringify(updateData);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", url+id, true);
+        xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+        xhr.onload = function () {
+            var res = JSON.parse(xhr.responseText);
+            if (xhr.readyState == 4 && xhr.status == "200") {
+                errorMessage.innerHTML = '';
+                responseP.removeAttribute('hidden');
+                responseP.innerHTML = 'The post was updated to show the following';
+                responseTitle.innerHTML = res.title;
+                responseBody.innerHTML = res.body;
+            } else {
+                errorMessage.innerHTML = 'error updating data';;
+            }
+        }
+        xhr.send(json);
+    }
+    
+})
